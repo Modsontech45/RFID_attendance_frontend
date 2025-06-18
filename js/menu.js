f46551e016b5bfcf6d0a7eeef663bfacee0ac468
ModsonTech45 <tandemodson55@gmail.com>
2025-06-18 15:14:16 +0000

    media query

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,43 @@
 const menuBtn = document.getElementById('menuBtn');
 const navMenu = document.getElementById('navMenu');
+const menuBtn2 = document.getElementById('menuBtn2');
+const navMenu2 = document.getElementById('navMenu2');
 
 menuBtn.addEventListener('click', () => {
   navMenu.classList.toggle('mobile-active');
 });
 
-// Optional: close menu when clicking outside or on a link
+
 navMenu.querySelectorAll('a, button.logoutBtn').forEach(link => {
   link.addEventListener('click', () => {
     navMenu.classList.remove('mobile-active');
   });
 });
 
-// Optional: close menu if clicked outside navMenu
+
 document.addEventListener('click', (e) => {
   if (!navMenu.contains(e.target) && e.target !== menuBtn) {
     navMenu.classList.remove('mobile-active');
+  }
+});
+
+
+
+//student
+menuBtn2.addEventListener('click', () => {
+  navMenu2.classList.toggle('mobile-active');
+});
+
+// Close menu when clicking links or logout button inside nav
+navMenu2.querySelectorAll('a, button.logoutBtn').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu2.classList.remove('mobile-active');
+  });
+});
+
+// Optional: Close nav if clicked outside nav and not on menu button
+document.addEventListener('click', (e) => {
+  if (!navMenu2.contains(e.target) && e.target !== menuBtn2) {
+    navMenu2.classList.remove('mobile-active');
   }
 });
