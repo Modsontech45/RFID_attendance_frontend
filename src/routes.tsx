52c69b32e67f1fe8dde699631bee332a6f712107
Modson Tande <131668620+Modsontech45@users.noreply.github.com>
2025-07-21 10:51:42 +0000

    reference

--- src/routes.tsx
+++ src/routes.tsx
@@ -54,7 +54,7 @@ export const adminRoutes = [
   { path: "/admin/reports", element: <Layout><ReportsPage /></Layout> },
   { path: "/admin/teacher/:teacherId", element: <Layout><AdminTeacherProfileView /></Layout> },
   { path: "/admin/time-settings", element: <Layout><TimeSettings /></Layout> },
-  { path: "/admin/verify-payment/:reference", element: <Layout><VerifyPayment /></Layout> },
+  { path: "/admin/verify-payment", element: <Layout><VerifyPayment /></Layout> },
 
 
 
